3da3d51581b5c02a975b8fcc9070c706c1df0ab9
Shashank Rammoorthy <shashankr.blr@gmail.com>
2016-11-01 12:33:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+})
 app.get('/ui/madi.png', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/ui/main.js', function(req, res) {
